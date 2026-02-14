3cc452d57d054a4392fefb6d5149c5a1029d393a
Daniel Schultz <dnltz@aesc-silicon.de>
2026-02-14 09:00:40 +0100

    hardware: i2cgpioexpander: SG13x: Reorder GPIO pins
    Order GPIO pins that they appear as GPIO 0-7 in the pin-out
    table, which is organized from clock-wise.
    
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- hardware/scala/i2cgpioexpander/SG13CMOS5L/SG13CMOS5L.scala
+++ hardware/scala/i2cgpioexpander/SG13CMOS5L/SG13CMOS5L.scala
@@ -23,14 +23,14 @@ case class SG13CMOS5LTop(p: I2cGpioExpander.Parameter, resetDelay: Int) extends 
       val interrupt = IhpCmosIo("south", 4, "clk_core")
     }
     val gpio = Vec(
+      IhpCmosIo("west", 0, "clk_core"),
+      IhpCmosIo("west", 1, "clk_core"),
+      IhpCmosIo("west", 2, "clk_core"),
       IhpCmosIo("north", 0, "clk_core"),
       IhpCmosIo("north", 1, "clk_core"),
       IhpCmosIo("north", 2, "clk_core"),
       IhpCmosIo("north", 3, "clk_core"),
-      IhpCmosIo("north", 4, "clk_core"),
-      IhpCmosIo("west", 0, "clk_core"),
-      IhpCmosIo("west", 1, "clk_core"),
-      IhpCmosIo("west", 2, "clk_core")
+      IhpCmosIo("north", 4, "clk_core")
     )
     val address = Vec(
       IhpCmosIo("east", 2, "clk_core"),
